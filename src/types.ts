bd33cde67ea9a66ce90d2549f22e2c6b491f8338
MrJackdaw <MrJackdaw@users.noreply.github.com>
2021-12-07 05:16:31 -0800

    * hotfix extended type refs

--- src/types.ts
+++ src/types.ts
@@ -33,7 +33,7 @@ export type PaginatedDBResults<T> = {
   page?: number;
 };
 
-export type ADIDBInterface<T> = Record<string, (a?: any) => any> & {
+export type ADIDBInterface<T> = Record<string, (...a: any[]) => any> & {
   listItems(opts: ListQueryOpts): Promise<PaginatedDBResults<T>>;
   getItem(id: any): Promise<T | null>;
   putItem(id: any, val: any): Promise<any | null>;
