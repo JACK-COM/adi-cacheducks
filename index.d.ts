7b9ae54d77b45e8793b543ad84a8ff468a3a46ab
MrJackdaw <MrJackdaw@users.noreply.github.com>
2021-12-07 04:29:39 -0800

    BAMP 107 * corrected fallback parameter for adi.list-items * adds paginated query/response protocol

--- index.d.ts
+++ index.d.ts
@@ -11,28 +11,45 @@ declare type KeyValConsumer = {
 /** Map of DBs (or other caches) and their CRUD interfaces */
 declare type ADICacheDBMap = { [name: string]: ADIDBInterface<any> };
 
+/** Query options for listing all items in a db */
+declare type ListQueryOpts = {
+  page?: number;
+  resultsPerPage?: number;
+  orderBy?: string;
+};
+
+/** Query results for a "List items" request (allows pagination) */
+declare type PaginatedDBResults<T> = {
+  totalResults?: number;
+  totalPages?: number;
+  resultsPerPage?: number;
+  data: T[];
+  page?: number;
+};
+
 /** CRUD interface for a single database or other cache source */
 declare type ADIDBInterface<T> = {
-  listItems(): Promise<T[]>;
+  listItems(opts?: ListQueryOpts): Promise<PaginatedDBResults<T>>;
   getItem(id: any): Promise<T | null>;
-  putItem(id: any, val: T): Promise<T | null>;
+  putItem(id: any, val: any): Promise<any | null>;
   removeItem(id: any): Promise<any>;
 };
 
 /** Data Interface (`ADI`) instance */
 declare type AppDataInterface = {
-  /** Write an incoming value to the supplied `cache` implementation, or remove any existing value with the supplied `key` if `value` is falsy (`undefined` or `null`). */
+  /** Write an incoming value to the supplied `cache`, or remove the supplied `key` if `value` is falsy (`undefined` or `null`). */
   cacheItem(key: string, value: any, cacheKey?: string): any;
-  /** Write an incoming value to the supplied `cache` implementation, or remove any existing value with the supplied `key` if `value` is falsy (`undefined` or `null`). */
+  /** Write incoming values to their respective `caches`, or remove the supplied `key` if `items[x].value` is falsy (`undefined` or `null`). */
   cacheMultiple(items: CacheItemArgs[]): void;
   /** Asserts whether the `ADI` instance has been initialized with a call to` onApplicationStart()`. */
   isInitialized(): boolean;
   /** Retrieve a list from the specified cache. */
-  listItems(cacheKey: string, fallback: () => Promise<any[]>): Promise<any[]>;
+  listItems(cacheKey: string, fallback?: () => Promise<any[]>): Promise<any[]>;
   /** Reset `ADI` to pre-initialized state. Disables reading from/writing to cache: use on [ user disconnect, app pause, etc ] */
   onApplicationEnd(): void;
   /** Initializes the `ADI` and gets it ready for front-end (or other app) interaction */
   onApplicationStart(): void;
+  /** Retrieve (or optionally fetch, cache, and return) data from a db/cache */
   publishItem(
     key: string,
     cacheKey?: string,
@@ -46,7 +63,7 @@ declare type AppDataInterface = {
   subscribeToCaches(
     listener: KeyValConsumer,
     caches: string[],
-    withinBounds?: (updatedKey: string, newVal: any, cache?: string) => boolean
+    withinBounds?: (updatedKey: string, newVal?: any, cache?: string) => boolean
   ): Unsubscriber;
 };
 
