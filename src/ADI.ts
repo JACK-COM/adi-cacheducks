d970e5df870b86164158de9cd032946f95dbc70f
MrJackdaw <MrJackdaw@users.noreply.github.com>
2022-01-16 19:27:32 -0800

    1.0.11 | Updated Typedefs (#19)
    * 1.0.11
    * updated typedefs
    * * prevent 'publish-all' on localStorage
    * * updates README
    * * updates 'subscribeToCache' typedef

--- src/ADI.ts
+++ src/ADI.ts
@@ -72,6 +72,7 @@ export async function publishItems(
   opts: ListQueryOpts,
   fallback = () => Promise.resolve([] as any)
 ) {
+  if (!opts.cacheKey) publishError("Can't publish all items from LocalStorage");
   const data = await listItems(opts, fallback);
   notifyAll("all", data, opts.cacheKey);
 }
@@ -80,7 +81,10 @@ export async function publishItems(
 export function removeItem(key: string, cacheKey?: string) {
   if (!initialized) {
     publishError("ADI is not initialized");
-  } else cache?.removeItem(key, cacheKey);
+  } else {
+    cache?.removeItem(key, cacheKey);
+    if (cacheKey) publishItems({ cacheKey });
+  }
 }
 
 export function onApplicationEnd() {
